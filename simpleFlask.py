8905817ccff68c2bd4449228eaf77001c2d2f819
lokesh <lokeshnano@gmail.com>
2023-09-06 03:41:39 +0530

    added new gui ele

--- simpleFlask.py
+++ simpleFlask.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "welcome to vercel flask deployment"
+@app.route("/<int:celsius>")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return str(fahrenheit)
 
 if __name__ == "__main__":
     app.run( debug=True)
